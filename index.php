3c63a0002d7e6ca4feab16435441353503775cfa
EriSilva.net <erisilva@erisilva.net>
2017-08-19 01:56:46 -0300

    commit 1.10 - segurança de acesso as pastas - correção

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <meta charset="UTF-8">
+    <meta name="author" content="Erivelton da Silva">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="robots" content="noindex, nofollow">
+    <link rel="icon" href="favicon.ico">
+    <body>
+  
+    </body>
+</html>
